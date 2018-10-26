ec726038f1cd7d3b9c05f060fba96b461fad794c
Justin Honegger <orestes.moiran@gmail.com>
2018-10-26 15:51:47 -0700

    giphy api working and correct gifs appearing

--- Assets/javascript/logic.js
+++ Assets/javascript/logic.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
     var topics = ["anger", "sadness", "happiness", "fear", "disgust", "surprise"];
 
     function displayTheGifs() {
-
+        $('#imagezone').empty();
         var emotion = $(this).attr("data-name");
         var seekrit = "2pJIZ9daipQ499TILv5WfCwuxuPUZq2W"
         // fix this
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + emotion + "&" + seekrit;
+        var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=" + seekrit + "&q=" + emotion + "&limit=10";
 
         // also fix this
         $.ajax({
@@ -15,11 +15,18 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
 
-            var imgurl = response.url;
-            var gifDiv = $("<div class='emotion");
-            var image = $("<img>").attr("src", imgurl)
-            gifDiv.append(image);
-            $('#imagezone').prepend(gifDiv);
+            var results = response.data;
+            console.log(response);
+            console.log(queryURL);
+            for (var i = 0; i < results.length; i++) {
+                var imgurl = results[i].images.fixed_height.url;
+                var gifDiv = $("<div class='emotion'>");
+                var image = $("<img>").attr("src", imgurl);
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                gifDiv.append(image);
+                gifDiv.append(p);
+                $('#imagezone').prepend(gifDiv);
+            }
         });
 
 
@@ -29,13 +36,13 @@ $(document).ready(function () {
         $("#buttonzone").empty();
         for (var i = 0; i < topics.length; i++) {
             var a = $("<button>");
-            a.addClass("btn btn-primary emotion");
+            a.addClass("btn btn-primary emotion " + topics[i]);
             a.attr("data-name", topics[i]);
             a.text(topics[i]);
             $('#buttonzone').append(a);
         }
     }
-    
+
     $(document).on("click", ".emotion", displayTheGifs);
     buttonsCreate();
 });
